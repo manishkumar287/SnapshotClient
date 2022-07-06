673e2f4c44e4bfed020d447bda944090479e03c7
manishkumar287 <manishkumarchaudhary287@gmail.com>
2022-07-06 16:10:18 +0530

    client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from 'react';
+import {Grow} from "@mui/material";
+import Container from "@mui/material/Container";
+import { Typography } from '@mui/material';
+import { Grid } from '@mui/material';
+import AppBar from "@mui/material/AppBar";
+import { useDispatch } from 'react-redux';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+import memories from './components/images/memories.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentId,setCurrentId]=useState(null);
+    const dispatch = useDispatch();
+    const classes = useStyles();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [currentId,dispatch]);
+    
+
+    return (
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            Snapshot
+          </Typography>
+          <img
+            className={classes.image}
+            src={memories}
+            alt="Memories"
+            height="60"
+          />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              className={classes.mainContainer}
+              justify="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid item xs={12} sm={7}>
+                <Posts setCurrentId={setCurrentId} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
+    );
 }
 
 export default App;
